e103659a50ddea11a58fe7af72c825c89d84b273
luis gustavo de medeiro borges <luisguga400@gmail.com>
2025-09-30 09:22:22 -0300

    versão 16

--- nodejs/modulos/calculadora/app.js
+++ nodejs/modulos/calculadora/app.js
@@ -1,14 +1,11 @@
-const funcoesMatematicas = require('./funcoes_matematicas');
-const calculadora_idade = require('./calcudara_idade');
-const resultadoSoma = funcoesMatematicas.somar(5,3);
-console.log('Resultado da soma: ${resultadoSoma}');
 
-const resultadoSubtracao = funcoesMatematicas.suntrair(10,4);
-console.log('Resultado da soma: ${resultadoSubtracao}');
+import {somar,suntrair,multiplicar,dividir} from './modulos/funcoes_matematicas.js';
+import calculadora_idade from 'calcudara_idade.js';
 
-const resultadoMultiplicacao = funcoesMatematicas.multiplicar(7,7);
-console.log('Resultado da soma: ${resultadoMultiplicacao}');
 
-const resultadodividir = funcoesMatematicas.dividir(7,7);
-console.log('Resultado da soma: ${resultadodividir}');
-console.log(calculadora_idade.idade("joão",2004,2050))
+console.log ("soma:" + somar(3,6,9));
+console.log ("subtração:" + suntrair(5,3));
+console.log ("multiplicação:" + multiplicar(5,3));
+console.log ("divisão:" + dividir(6,3));
+
+console.log(calculadora_idade.idade("joão",1979,2050));
